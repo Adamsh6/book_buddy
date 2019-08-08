55289f5ece857203f89686ca8d234b12e3b28f49
Adamsh6 <adam.s.houten@gmail.com>
2019-08-08 15:35:26 +0100

    added MainContainer

---
+++ client/book_buddy/src/containers/MainContainer.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class MainContainer extends Component {
+  render(){
+    return <h1>This is the main container</h1>
+  }
+}
+
+export default MainContainer
