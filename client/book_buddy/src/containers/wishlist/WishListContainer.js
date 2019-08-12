991a4b5d79fecb036fd49b649478406d6e370917
Adamsh6 <adam.s.houten@gmail.com>
2019-08-12 15:06:03 +0100

    commented out some useless code

--- client/book_buddy/src/containers/wishlist/WishListContainer.js
+++ client/book_buddy/src/containers/wishlist/WishListContainer.js
@@ -5,28 +5,26 @@ import Request from '../../helpers/request'
 class WishListContainer extends Component {
   constructor(props){
     super(props)
-    if(this.props.user){
-    this.state = {
-    userWishlist: this.props.user.wishlist
-    };
-    console.log(this.props.user.wishlist)
-  }
+    // if(this.props.user){
+    // this.state = {
+    // userWishlist: this.props.user.wishlist
+    // };
+  // }
 
     this.handleAddToWishList = this.handleAddToWishList.bind(this)
   }
 
 
 
-componentDidUpdate(prevProps) {
-  if(prevProps.user.wishlist !== this.state.userWishlist) {
-    this.setState({userWishlist: this.props.user.wishlist});
-  }
-}
+// componentDidUpdate(prevProps) {
+//   if(prevProps.user.wishlist !== this.state.userWishlist) {
+//     this.setState({userWishlist: this.props.user.wishlist});
+//   }
+// }
 
   handleAddToWishList(id, payload){
     const request = new Request();
     const userUrl = '/api/users/' + id
-    // const currentUser = this.props.user
     request.patch(userUrl, payload)
     .then(() => this.props.getAllData())
 
